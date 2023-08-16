48ebeca00171b95edc580b49c12f64d9fae801a2
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-08-16 07:08:01 -0300

    fixing images not loading

--- src/data.js
+++ src/data.js
@@ -2,28 +2,28 @@ export const projectInfos = [
 	{
 		category: "Reaction",
 		score: 80,
-		icon: "../src/assets/images/icon-reaction.svg",
+		icon: "/assets/images/icon-reaction.svg",
 		fontColor: "hsl(0, 100%, 67%)",
 		bgColor: "hsla(0, 100%, 67%,0.1)"
 	},
 	{
 		category: "Memory",
 		score: 92,
-		icon: "../src/assets/images/icon-memory.svg",
+		icon: "/assets/images/icon-memory.svg",
 		fontColor: "hsl(39, 100%, 56%);",
 		bgColor: "hsla(39, 100%, 56%,0.1)"
 	},
 	{
 		category: "Verbal",
 		score: 61,
-		icon: "./src/assets/images/icon-verbal.svg",
+		icon: "/assets/images/icon-verbal.svg",
 		fontColor: "hsl(166, 100%, 37%)",
 		bgColor: "hsla(166, 100%, 37%,0.1)"
 	},
 	{
 		category: "Visual",
 		score: 72,
-		icon: "./src/assets/images/icon-visual.svg",
+		icon: "/assets/images/icon-visual.svg",
 		fontColor: "hsl(234, 85%, 45%)",
 		bgColor: "hsla(234, 85%, 45%,0.1)"
 	}
